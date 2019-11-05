1c856f1adc86d41ce636130a25ca61ac7ecd2afe
David <Kanter.Dev@gmail.com>
2019-11-05 13:18:09 -0500

    Init Commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoadingScreen from "./views/LoadingScreen/view";
+
+export default function App() {
+  return (
+    <View>
+      <LoadingScreen></LoadingScreen>
+    </View>
+  );
+}
+
