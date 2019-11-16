cf3f8d75b7c29f5857986543e64ab77d90f48ae7
Jacky He <heja@utschools.ca>
2019-11-15 19:37:13 -0500

    I am really bad at commiting, forgot to add the files

--- views/DayNightScreen/view.js
+++ views/DayNightScreen/view.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
-import { Animated, Image, View } from 'react-native';
+import { Animated, Image, View, Text} from 'react-native';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
 
 import style from "./style";
+
+const images = {
+    dayImage: require ('./assets/dayImage.png'),
+    nightImage: require ('./assets/nightImage.png'),
+};
 
 class DayNightScreen extends Component {
     style = style;
     render()
     {
-        return (<View/>);
+        return (
+        <View style = {{flex: 1, flexDirection: 'column'}}>
+            <Image style = {style.nightImage} source = {images.nightImage}/>
+            <Image style = {style.dayImage} source = {images.dayImage}/>
+            <Text style = {style.orText}>
+                OR
+            </Text>
+        </View>
+        );
     }
 }
 
